4489e77336767c5833b20518bee8c7b4eac3a6f2
humbertoguerrer <humberto.guerrer@gmail.com>
2021-04-20 16:01:26 -0300

    Projeto criado

---
+++ src/main/java/com/hgn/finchhamburgueria/FinchhamburgueriaApplication.java
@@ -0,0 +1,13 @@
+package com.hgn.finchhamburgueria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinchhamburgueriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinchhamburgueriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hgn/finchhamburgueria/FinchhamburgueriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgn.finchhamburgueria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinchhamburgueriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
